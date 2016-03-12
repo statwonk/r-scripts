f934316d1d3bb4709dde2e742bc7caa4f0b55793
statwonk <cpeter9@gmail.com>
2016-03-12 10:51:25 -0600

    Some context on the sys.calls function and its use to give human context to an error.

--- conditions.R
+++ conditions.R
@@ -1,8 +1,14 @@
 # Source: http://adv-r.had.co.nz/Exceptions-Debugging.html#condition-handling
 
 condition <- function(subclass, message,
-                      call = sys.call(-2), # -2 ?? -3 seems to produce a clearer error
-                      ...) {
+  # "This is shown here with sys.calls(), which is the run-time equivalent of traceback() —
+  # it lists all calls leading to the current function."
+
+  # "sys.call, sys.frame and sys.function accept integer values for the argument
+  #  which. Non-negative values of which are frame numbers whereas negative
+  #  values are counted back from the frame number of the current evaluation."
+  call = sys.call(-1),
+  ...) {
   # Called inside "error catching" function, condition captures
   # context of the caller and attach the caller's frame as an
   # attribute.
